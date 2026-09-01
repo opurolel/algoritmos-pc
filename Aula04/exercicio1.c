0565705bfd7456676ae405d64400d20c3a3997f6
Leonardo Delmondes <leo.delmondesc@gmail.com>
2026-09-01 19:17:25 -0300

    Aula04ex1correção

--- Aula04/exercicio1.c
+++ Aula04/exercicio1.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 int main() {
-  setlocale(LC_CTYPE, "");
-  float nota1, nota2, media;
-  printf("Digite a nota 1: ");
-  scanf("%f", &nota1);
-  printf("Digite a nota 2: ");
-  scanf("%f", &nota2);
-      media = (nota1+nota2)/2;
-  if(media >= 6) {
-  printf("Parabéns! Você foi aprovado com a média %.2f", media);
-      } 
-      }
-  else {
-  printf("Você foi reprovado!");
-  return 0;
-}
+setlocale(LC_CTYPE, "");
+float a, b, c, x1, x2, delta;
+printf("Qual o valor de a?");
+scanf("%f", &a);
+printf("Qual o valor de b?");
+scanf("%f", &b);
+printf("Qual o valor de c?");
+scanf("%f", &c);
+    delta = pow(b,2) - 4*a*c;
+if(delta >= 0) {
+        x1 
+= (-b + sqrt(delta)) / (2*a);
+        x2 
+= (-b - sqrt(delta)) / (2*a);
+printf("\nRaízes da equação:");
+printf("\nx1 = %.2f", x1);
+printf("\nx2 = %.2f", x2);
+    } 
+    }
+else {
+printf("Não existem raízes reais!!!");
+return 0;
